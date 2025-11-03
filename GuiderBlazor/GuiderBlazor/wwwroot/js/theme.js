0fc6284ba407f450194b4545bab61ce7ef954887
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-11-03 19:08:22 +0800

    Dark/Light themes

---
+++ GuiderBlazor/GuiderBlazor/wwwroot/js/theme.js
@@ -0,0 +1,15 @@
+﻿// Файл: wwwroot/js/theme.js
+
+// 1. Применяет тему (light-theme или dark-theme) к <html>
+function applyTheme(theme) {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+}
+
+// 2. Загружает сохраненную тему при запуске
+function loadTheme() {
+    var theme = localStorage.getItem('theme') || 'light-theme'; // По умолчанию 'light'
+    document.documentElement.className = theme;
+    // Возвращаем true, если тема темная, чтобы Blazor знал
+    return theme === 'dark-theme';
+}
