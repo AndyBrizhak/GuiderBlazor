81cec773477ae18f96986d6907648f0319b3d09f
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-11-16 12:29:28 +0800

    add geo near

---
+++ GuiderBlazor/GuiderBlazor/wwwroot/js/location.js
@@ -0,0 +1,25 @@
+﻿// wwwroot/js/location.js
+
+window.getBrowserLocation = (dotNetHelper) => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                // Success: Call C# method 'SetLocation'
+                dotNetHelper.invokeMethodAsync('SetLocation',
+                    position.coords.latitude,
+                    position.coords.longitude);
+            },
+            (error) => {
+                // Error: Call C# method 'LocationError'
+                dotNetHelper.invokeMethodAsync('LocationError',
+                    error.code,
+                    error.message);
+            }
+        );
+    } else {
+        // Error: Browser doesn't support Geolocation
+        dotNetHelper.invokeMethodAsync('LocationError',
+            -1, // Custom code for "not supported"
+            'Geolocation is not supported by this browser.');
+    }
+};
