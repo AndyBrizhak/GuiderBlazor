7c437c127fafdc1b7f0ff08df41e32ec7e5e770a
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-11-23 11:14:55 +0800

    add sitemap

--- GuiderBlazor/GuiderBlazor/Program.cs
+++ GuiderBlazor/GuiderBlazor/Program.cs
@@ -1,6 +1,6 @@
 using GuiderBlazor.Components;
 using GuiderBlazor.Shared.Services;
-//using Shared.Services;
+using GuiderBlazor.Shared.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -44,5 +44,37 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(GuiderBlazor.Client._Imports).Assembly);
+
+
+app.MapGet("/sitemap.xml", async () =>
+{
+    // 1. Настройки адресов
+    // Адрес API (берем тот же, что у вас в builder)
+    var apiBaseUrl = "https://localhost:8081/";
+    // Адрес вашего сайта (для локальной разработки)
+    var siteBaseUrl = "http://localhost:3000";
+
+    List<string> slugs = new();
+
+    // 2. Создаем отдельный HttpClient, так как мы вне scope обычного пользователя
+    using (var client = new HttpClient { BaseAddress = new Uri(apiBaseUrl) })
+    {
+        try
+        {
+            // Запрашиваем у API только список слагов (строк)
+            slugs = await client.GetFromJsonAsync<List<string>>("sitemap/places-slugs") ?? new();
+        }
+        catch
+        {
+            // Игнорируем ошибки API, чтобы хотя бы главная страница попала в sitemap
+        }
+    }
+
+    // 3. Генерируем XML, вызывая статический метод из проекта Shared
+    var xmlContent = SitemapLogic.Generate(siteBaseUrl, slugs);
+
+    // 4. Отдаем XML
+    return Results.Text(xmlContent, "application/xml");
+});
 
 app.Run();
---
+++ Shared/Utils/SitemapLogic.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Linq;
+
+namespace GuiderBlazor.Shared.Utils
+{
+    public static class SitemapLogic
+    {
+        public static string Generate(string siteBaseUrl, IEnumerable<string> placeSlugs)
+        {
+            XNamespace xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+            var root = new XElement(xmlns + "urlset");
+
+            // 1. Главная страница
+            root.Add(new XElement(xmlns + "url",
+                new XElement(xmlns + "loc", $"{siteBaseUrl}/"),
+                new XElement(xmlns + "changefreq", "daily"),
+                new XElement(xmlns + "priority", "1.0")
+            ));
+
+            // 2. Страницы мест (Places)
+            if (placeSlugs != null)
+            {
+                foreach (var slug in placeSlugs)
+                {
+                    root.Add(new XElement(xmlns + "url",
+                        new XElement(xmlns + "loc", $"{siteBaseUrl}/place/{slug}"),
+                        new XElement(xmlns + "changefreq", "weekly"),
+                        new XElement(xmlns + "priority", "0.8")
+                    ));
+                }
+            }
+
+            var doc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"), root);
+            return doc.Declaration + Environment.NewLine + doc.ToString();
+        }
+    }
+}
