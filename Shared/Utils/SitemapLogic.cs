fcea806f422586046d56a9f717d1a0b0fdec1b64
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-12-13 15:40:19 +0800

    upgrade sitemap generation  logic in  utils

--- Shared/Utils/SitemapLogic.cs
+++ Shared/Utils/SitemapLogic.cs
@@ -1,10 +1,12 @@
-﻿using System.Xml.Linq;
+﻿using System.Text.Json; // Для JsonElement
+using System.Xml.Linq;
 
 namespace GuiderBlazor.Shared.Utils
 {
     public static class SitemapLogic
     {
-        public static string Generate(string siteBaseUrl, IEnumerable<string> placeSlugs)
+        // Принимаем JsonElement, так как API отдает массив объектов
+        public static string Generate(string siteBaseUrl, JsonElement items)
         {
             XNamespace xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9";
             var root = new XElement(xmlns + "urlset");
@@ -13,19 +15,31 @@ namespace GuiderBlazor.Shared.Utils
             root.Add(new XElement(xmlns + "url",
                 new XElement(xmlns + "loc", $"{siteBaseUrl}/"),
                 new XElement(xmlns + "changefreq", "daily"),
-                new XElement(xmlns + "priority", "1.0")
+                new XElement(xmlns + "priority", "1.0"),
+                new XElement(xmlns + "lastmod", DateTime.UtcNow.ToString("yyyy-MM-dd"))
             ));
 
-            // 2. Страницы мест (Places)
-            if (placeSlugs != null)
+            // 2. Страницы мест
+            if (items.ValueKind == JsonValueKind.Array)
             {
-                foreach (var slug in placeSlugs)
+                foreach (var item in items.EnumerateArray())
                 {
-                    root.Add(new XElement(xmlns + "url",
-                        new XElement(xmlns + "loc", $"{siteBaseUrl}/place/{slug}"),
-                        new XElement(xmlns + "changefreq", "weekly"),
-                        new XElement(xmlns + "priority", "0.8")
-                    ));
+                    // Парсим JSON объект без создания классов
+                    // Ожидаем формат: { "url": "slug", "lastMod": "2024-12-13" }
+
+                    string slug = item.GetProperty("url").GetString() ?? "";
+                    string date = item.GetProperty("lastMod").GetString() ?? DateTime.UtcNow.ToString("yyyy-MM-dd");
+
+                    if (!string.IsNullOrEmpty(slug))
+                    {
+                        var urlElement = new XElement(xmlns + "url",
+                            new XElement(xmlns + "loc", $"{siteBaseUrl}/place/{slug}"),
+                            new XElement(xmlns + "changefreq", "weekly"),
+                            new XElement(xmlns + "priority", "0.8"),
+                            new XElement(xmlns + "lastmod", date) // <-- Вставляем дату из базы
+                        );
+                        root.Add(urlElement);
+                    }
                 }
             }
 
